461b8c2f4f3ee074279fbc2d95ff9dfe01280ba5
wzq <18837129070@163.com>
2020-02-25 16:46:53 +0800

    上传基本环境

---
+++ src/main/java/com/baizhi/testideagit/TestIdeaGitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.testideagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIdeaGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestIdeaGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/testideagit/TestIdeaGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baizhi.testideagit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestIdeaGitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
